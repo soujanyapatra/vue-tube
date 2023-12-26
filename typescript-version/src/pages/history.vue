2c5cd82550a62e6b6f8f7c806ed262c99f25dccb
Soujanya patra <98420368+soujanyapatra@users.noreply.github.com>
2023-12-26 19:20:12 +0530

    Delete typescript-version directory
    not required

--- typescript-version/src/pages/history.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <div>history</div>
-</template>
--- typescript-version/src/pages/watch-later.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <div>watch later</div>
-</template>
